75e3478e794430dbe84e573a7fc6bc1adb24f79d
kookpa <141832427+kookpa@users.noreply.github.com>
2023-11-12 14:47:11 -0300

    Add files via upload

---
+++ senha/senha/senha/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace senha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.btnADD = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.Location = new System.Drawing.Point(104, 62);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(122, 20);
+            this.txbSenha.TabIndex = 1;
+            this.txbSenha.TextChanged += new System.EventHandler(this.txbSenha_TextChanged);
+            // 
+            // btnADD
+            // 
+            this.btnADD.Location = new System.Drawing.Point(106, 116);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(77, 50);
+            this.btnADD.TabIndex = 2;
+            this.btnADD.Text = "Adicionar";
+            this.btnADD.UseVisualStyleBackColor = true;
+            this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(209, 116);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(69, 50);
+            this.btnRemover.TabIndex = 3;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnADD);
+            this.Controls.Add(this.txbSenha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.Button btnADD;
+        private System.Windows.Forms.Button btnRemover;
+    }
+}
+
---
+++ senha/senha/senha/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace senha
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static class PasswordGenerator
+        {
+            private static readonly Random random = new Random();
+            public static string Generate(int length, bool includeLetters, bool includeNumbers, bool includeSpecialChars)
+            {
+                const string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                const string numbers = "0123456789";
+                const string specialChars = "!@#$%^&*()_+-=[]{};:,.<>?";
+
+                var chars = string.Concat(
+               includeLetters ? letters : "",
+               includeNumbers ? numbers : "",
+               includeSpecialChars ? specialChars : "");
+
+                return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+            }
+            
+        }
+
+        private void txbSenha_TextChanged(object sender, EventArgs e)
+        {
+            var password = PasswordGenerator.Generate(12, true, true, true);
+            TextBoxManager.btnAdd(password, txbSenha);
+            TextBoxManager.btnRemover(5, txbSenha);
+        }
+        public static class TextBoxManager
+        {
+            public static void btnAdd(string btnAdd, System.Windows.Forms.TextBox txbSenha)
+            {
+                txbSenha.Text += btnAdd;
+            }
+            public static void btnRemover(int btnRemover, System.Windows.Forms.TextBox txbSenha)
+            {
+                if (btnRemover > txbSenha.Text.Length)
+                {
+                    throw new ArgumentException("Length to remove is greater than the length of the text in the TextBox.");
+                }
+                txbSenha.Text = txbSenha.Text.Substring(0, txbSenha.Text.Length - btnRemover);
+            }
+        }
+    }
+}
